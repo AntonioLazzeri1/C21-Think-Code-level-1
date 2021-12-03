df62cb3e6ae10670bdedb9cafd7dc0b4d49b9668
antonio <voetballergamerajdo@gmail.com>
2021-12-03 10:22:08 +0100

    fouten verbeterd

--- Opdracht2/opdracht2.py
+++ Opdracht2/opdracht2.py
@@ -1,9 +1,9 @@
 # syntax error
-print(Hello world)
+print("Hello world!")
 
 # runtime error
-10 * (2/0)
+10 * (2/5)
 
 # semantic error
-name = "Alice"
-print("Hello name")
+name = "Antonio"
+print("Hello Antonio!")
