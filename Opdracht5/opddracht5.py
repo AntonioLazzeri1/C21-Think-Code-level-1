8424bd5eb293527dd8a1eed19a1e60d88004b348
antonio <voetballergamerajdo@gmail.com>
2022-01-25 12:01:01 +0100

    klaar

---
+++ Opdracht5/opddracht5.py
@@ -0,0 +1,11 @@
+ def dingen_wat_ik_leuk_vind_om_te_doen (name):   
+        print("een van de dingen die leuk vind om te doen is",name)
+dingen_wat_ik_leuk_vind_om_te_doen("games spelen")
+dingen_wat_ik_leuk_vind_om_te_doen("reizen")
+dingen_wat_ik_leuk_vind_om_te_doen("familie film kijken")
+a = 22
+b = 100
+if b > a:
+  print("b is greater than a")
+
+  
--- Opdracht5/opdracht_05.py
+++ Opdracht5/opdracht_05.py
@@ -3,8 +3,8 @@
 #weet je niet meer hoe? Check het solution filmpje op: https://www.linkedin.com/learning/programming-foundations-fundamentals-3/solution-favorite-cities
 def favoriete_games(name):
     print("een van mijn favorieten games zijn",name)
-    favoriete_games("fifa")
-    favoriete_games("fortnite")
-    favoriete_games("gta 5")
+favoriete_games("fifa")
+favoriete_games("fortnite")
+favoriete_games("gta 5")
     
    
