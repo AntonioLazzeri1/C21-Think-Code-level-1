e0fc0ca129477a9d2da123097f102bcd6980e08f
antonio <voetballergamerajdo@gmail.com>
2022-01-25 12:03:35 +0100

    klaar

--- Opdracht5/opddracht5.py
+++ Opdracht5/opddracht5.py
@@ -1,5 +1,5 @@
- def dingen_wat_ik_leuk_vind_om_te_doen (name):   
-        print("een van de dingen die leuk vind om te doen is",name)
+def dingen_wat_ik_leuk_vind_om_te_doen (name):   
+    print("een van de dingen die leuk vind om te doen is",name)
 dingen_wat_ik_leuk_vind_om_te_doen("games spelen")
 dingen_wat_ik_leuk_vind_om_te_doen("reizen")
 dingen_wat_ik_leuk_vind_om_te_doen("familie film kijken")
